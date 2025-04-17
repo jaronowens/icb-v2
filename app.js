f6558047a3547a9685aaa0850b0447f37bab9a5f
Jaron Owens <jaronowens@gmail.com>
2025-04-17 10:38:42 -0400

    add collections and httprequests

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const createError = require('http-errors');
 const express = require('express');
+const qs = require('qs');
+const fs = require('fs');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const collectionRouter = require('./routes/collection');
 
 const app = express();
+
+// set up cors and query parser
+app.use(cors());
+app.set('query parser'), function (str) {
+  return qs.parse(str);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +29,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/collection', collectionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
